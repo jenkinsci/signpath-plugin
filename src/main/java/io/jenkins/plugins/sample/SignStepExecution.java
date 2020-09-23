d82a0e1cd0b76597ce61e638a844c64552a5e816
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2020-09-23 16:33:39 +0200

    SIGN-3326 Now also read artifact from artifact manager. Cleanup code
    Former-commit-id: 56b3c37e7e71995e29e1d69b6017af75277132ce

--- src/main/java/io/jenkins/plugins/sample/SignStepExecution.java
+++ src/main/java/io/jenkins/plugins/sample/SignStepExecution.java
@@ -1,34 +1,27 @@
 package io.jenkins.plugins.sample;
 
-import com.cloudbees.plugins.credentials.*;
-import com.cloudbees.plugins.credentials.common.PasswordCredentials;
-import com.sun.xml.internal.rngom.parse.host.Base;
-import hudson.EnvVars;
-import hudson.Launcher;
+import com.cloudbees.plugins.credentials.CredentialsMatcher;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.plugins.git.util.BuildData;
 import hudson.security.ACL;
+import jenkins.model.ArtifactManager;
 import jenkins.model.Jenkins;
+import jenkins.util.VirtualFile;
+import org.apache.commons.io.IOUtils;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution;
-import org.w3c.dom.Document;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
-import java.io.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SignStepExecution extends SynchronousStepExecution {
 
@@ -42,15 +35,10 @@ public class SignStepExecution extends SynchronousStepExecution {
     @Override
     protected String run() throws Exception {
         TaskListener listener = getContext().get(TaskListener.class);
-        EnvVars vars = getContext().get(hudson.EnvVars.class);
-        Launcher launcher = getContext().get(Launcher.class);
         Run run = getContext().get(Run.class);
-
         PrintStream logger = listener.getLogger();
 
-        String hostname = resolveHostname();
-        logger.println("Running Sign Step for jenkins server: " + hostname);
-        logger.println("Parameters, organizationId:" + signStep.getOrganizationId() + " waitForCompletion: "+ signStep.getWaitForCompletion());
+        logger.println("signArtifact organizationId:" + signStep.getOrganizationId() + " waitForCompletion: "+ signStep.getWaitForCompletion());
 
         List<StringCredentials> credentials =
                 CredentialsProvider.lookupCredentials(StringCredentials.class, Jenkins.getInstanceOrNull(), ACL.SYSTEM, Collections.emptyList());
@@ -64,22 +52,17 @@ public class SignStepExecution extends SynchronousStepExecution {
         String sha1Hashes = buildData.getBuildsByBranchName().entrySet().stream().map(stringBuildEntry -> stringBuildEntry.getValue().getSHA1().toString()).collect(Collectors.joining());
         logger.println("remote urls:" + remoteUrls + " branch:"+branches+" sha1: "+sha1Hashes);
 
+        ArtifactManager artifactManager = run.getArtifactManager();
+        VirtualFile unsignedArtifact = artifactManager.root().child("Calculator\\bin\\Release\\netcoreapp3.1\\publish\\Calculator.deps.json");
+        if(!unsignedArtifact.exists()) {
+            throw new IllegalArgumentException("artifact file does not exist");
+        }
+
+        try(InputStream s = unsignedArtifact.open();){
+            String content = IOUtils.toString(s, StandardCharsets.UTF_8);
+            logger.println(content);
+        }
+
         return "something";
-    }
-
-    private String resolveHostname(){
-        String hostname="unresolved host";
-        try
-        {
-            InetAddress addr;
-            addr = InetAddress.getLocalHost();
-            hostname = addr.getHostName();
-        }
-        catch (UnknownHostException ex)
-        {
-            hostname = ex.toString();
-        }
-
-        return hostname;
     }
 }
