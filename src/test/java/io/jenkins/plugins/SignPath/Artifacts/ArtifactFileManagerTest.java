2d4bb6ccbb2487401919ae4b8bf48e8baaeb9c4c
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2020-09-30 17:13:15 +0200

    SIGN-3326 Adapt ArtifactFileManagerTest so it actually works (fully integrative via pipeline script)
    Former-commit-id: c826d8697a13db1c9be5db82672f115943a5a1ac

--- src/test/java/io/jenkins/plugins/SignPath/Artifacts/ArtifactFileManagerTest.java
+++ src/test/java/io/jenkins/plugins/SignPath/Artifacts/ArtifactFileManagerTest.java
@@ -1,57 +1,46 @@
 package io.jenkins.plugins.SignPath.Artifacts;
 
 import hudson.Launcher;
-import hudson.model.Result;
 import hudson.model.TaskListener;
-import hudson.model.queue.QueueTaskFuture;
 import io.jenkins.plugins.SignPath.Common.TemporaryFile;
+import io.jenkins.plugins.SignPath.TestUtils.SignPathJenkinsRule;
 import io.jenkins.plugins.SignPath.TestUtils.Some;
 import io.jenkins.plugins.SignPath.TestUtils.TemporaryFileUtil;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class ArtifactFileManagerTest {
     @Rule
-    public JenkinsRule j= new JenkinsRule();
+    public SignPathJenkinsRule j= new SignPathJenkinsRule();
 
     private ArtifactFileManager sut;
-    private WorkflowRun run;
 
     @Before
-    public void setup() throws IOException, ExecutionException, InterruptedException {
+    public void setup() throws Exception {
         Launcher launcher = j.createLocalLauncher();
         TaskListener listener = j.createTaskListener();
-        WorkflowJob workflowJob = j.createProject(WorkflowJob.class);
-        workflowJob.setDefinition(new CpsFlowDefinition(
-                "pipeline { stages { stage('test') { steps {"+
-                "writeFile text: 'hello', file: 'hello.txt'; " +
-                "step([$class: 'ArtifactArchiver', artifacts: 'hello.txt', fingerprint: true]) " +
-                        "} } } }",
-                true));
 
-        QueueTaskFuture<WorkflowRun> futureRun  = workflowJob.scheduleBuild2(0);
-        run = futureRun.get();
+        WorkflowJob workflowJob = j.createWorkflow("SignPath",
+                "writeFile text: 'hello', file: 'hello.txt'; " +
+                "archiveArtifacts artifacts: 'hello.txt', fingerprint: true ");
+
+        WorkflowRun run = j.assertBuildStatusSuccess(workflowJob.scheduleBuild2(0));
         sut = new ArtifactFileManager(run, launcher, listener);
     }
 
     @Test
-    public void retrieveArtifact() throws IOException, InterruptedException {
-        Result result= run.getResult();
-
+    public void retrieveArtifact() throws IOException {
         // ACT
-        TemporaryFile retrievedArtifact = sut.retrieveArtifact("greeting.txt");
+        TemporaryFile retrievedArtifact = sut.retrieveArtifact("hello.txt");
         byte[] retrievedArtifactContent = TemporaryFileUtil.getContent(retrievedArtifact);
         String retrievedArtifactString = new String(retrievedArtifactContent, StandardCharsets.UTF_8);
 
