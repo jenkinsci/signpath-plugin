68abae0b2b666e80b46e520075ac470e4d20776d
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2020-10-01 14:46:57 +0200

    SIGN-3326 Cleanup SubmitSigningRequestStepExecution and try to correctly implement it
    Former-commit-id: 42ed74966c04399cac415ed6a454384ac0b17e66

--- src/main/java/io/jenkins/plugins/SignPath/SubmitSigningRequestStepExecution.java
+++ src/main/java/io/jenkins/plugins/SignPath/SubmitSigningRequestStepExecution.java
@@ -1,25 +1,35 @@
 package io.jenkins.plugins.SignPath;
 
-import com.profesorfalken.jpowershell.PowerShell;
-import com.profesorfalken.jpowershell.PowerShellResponse;
 import hudson.Launcher;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import io.jenkins.plugins.SignPath.ApiIntegration.ApiConfiguration;
+import io.jenkins.plugins.SignPath.ApiIntegration.ISignPathFacade;
+import io.jenkins.plugins.SignPath.ApiIntegration.Model.SigningRequestModel;
+import io.jenkins.plugins.SignPath.ApiIntegration.Model.SigningRequestOriginModel;
+import io.jenkins.plugins.SignPath.ApiIntegration.PowerShell.PowerShellExecutor;
+import io.jenkins.plugins.SignPath.ApiIntegration.PowerShell.SignPathPowerShellFacade;
+import io.jenkins.plugins.SignPath.ApiIntegration.SignPathCredentials;
 import io.jenkins.plugins.SignPath.Artifacts.ArtifactFileManager;
 import io.jenkins.plugins.SignPath.Common.TemporaryFile;
+import io.jenkins.plugins.SignPath.Exceptions.OriginNotRetrievableException;
+import io.jenkins.plugins.SignPath.Exceptions.SecretNotFoundException;
+import io.jenkins.plugins.SignPath.Exceptions.SignPathFacadeCallException;
 import io.jenkins.plugins.SignPath.OriginRetrieval.DefaultConfigFileProvider;
 import io.jenkins.plugins.SignPath.OriginRetrieval.OriginRetriever;
-import io.jenkins.plugins.SignPath.ApiIntegration.Model.SigningRequestOriginModel;
 import io.jenkins.plugins.SignPath.SecretRetrieval.CredentialBasedSecretRetriever;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URL;
+import java.util.UUID;
 
-public class SubmitSigningRequestStepExecution extends SynchronousStepExecution {
+public class SubmitSigningRequestStepExecution extends SynchronousStepExecution<String> {
 
-    private SubmitSigningRequestStep signStep;
+    private final SubmitSigningRequestStep signStep;
 
     protected SubmitSigningRequestStepExecution(SubmitSigningRequestStep signStep, StepContext context) {
         super(context);
@@ -27,9 +37,11 @@ public class SubmitSigningRequestStepExecution extends SynchronousStepExecution 
     }
 
     @Override
-    protected String run() throws Exception {
+    protected String run() throws IOException, InterruptedException {
         TaskListener listener = getContext().get(TaskListener.class);
-        Run run = getContext().get(Run.class);
+        assert listener != null;
+
+        Run<?, ?> run = getContext().get(Run.class);
         Launcher launcher = getContext().get(Launcher.class);
         PrintStream logger = listener.getLogger();
         Jenkins jenkins = Jenkins.get();
@@ -41,16 +53,33 @@ public class SubmitSigningRequestStepExecution extends SynchronousStepExecution 
         CredentialBasedSecretRetriever credentialSecretRetriever = new CredentialBasedSecretRetriever(jenkins);
         OriginRetriever originRetriever = new OriginRetriever(new DefaultConfigFileProvider(run), run, jenkinsRootUrl);
         ArtifactFileManager artifactFileManager = new ArtifactFileManager(run, launcher, listener);
+        PowerShellExecutor pwsh = new PowerShellExecutor("pwsh");
 
-        String trustedBuildSystemToken = credentialSecretRetriever.retrieveSecret("TrustedBuildSystemToken");
-        SigningRequestOriginModel originModel = originRetriever.retrieveOrigin();
-        TemporaryFile unsignedArtifact = artifactFileManager.retrieveArtifact("Calculator\\bin\\Release\\netcoreapp3.1\\publish\\Calculator.deps.json");
+        ApiConfiguration apiConfiguration = new ApiConfiguration(new URL(signStep.getApiUrl()),
+                signStep.getServiceUnavailableTimeoutInSeconds(),
+                signStep.getUploadAndDownloadRequestTimeoutInSeconds(),
+                signStep.getWaitForCompletionTimeoutInSeconds());
 
-        try (PowerShell powerShell = PowerShell.openSession("pwsh.exe")) {
-            PowerShellResponse response = powerShell.executeSingleCommand("Get-Process");
-            logger.println("Get-Process Result:" + response.getCommandOutput());
+        try {
+            String trustedBuildSystemToken = credentialSecretRetriever.retrieveSecret("TrustedBuildSystemToken");
+            SignPathCredentials credentials = new SignPathCredentials(signStep.getCiUserToken(), trustedBuildSystemToken);
+            ISignPathFacade signPathFacade = new SignPathPowerShellFacade(pwsh, credentials, apiConfiguration);
+            SigningRequestOriginModel originModel = originRetriever.retrieveOrigin();
+            TemporaryFile unsignedArtifact = artifactFileManager.retrieveArtifact("Calculator\\bin\\Release\\netcoreapp3.1\\publish\\Calculator.deps.json");
+
+            TemporaryFile signedArtifact = signPathFacade.submitSigningRequest(new SigningRequestModel(
+                    UUID.fromString(signStep.getOrganizationId()),
+                    signStep.getProjectSlug(),
+                    signStep.getArtifactConfigurationSlug(),
+                    signStep.getSigningPolicySlug(),
+                    signStep.getDescription(),
+                    originModel,
+                    unsignedArtifact));
+
+            artifactFileManager.storeArtifact(signedArtifact, signStep.getOutputArtifactPath());
+            return "Signing step succeeded";
+        } catch (SecretNotFoundException | OriginNotRetrievableException | SignPathFacadeCallException ex) {
+            return "Signing step failed: " + ex.getMessage();
         }
-
-        return "something";
     }
 }
