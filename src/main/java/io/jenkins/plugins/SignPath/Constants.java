710b442c9103386fc51fbf035e7792c1bf14284e
Patrick Schwarz <patrick.schwarz@rubicon.eu>
2020-10-15 10:44:40 +0200

    SIGN-3415 Adjusted secret id with PSA
    Former-commit-id: 43a117667e2ad7a58bb4eeb8a8817a9a1ca7b8f1

--- src/main/java/io/jenkins/plugins/SignPath/Constants.java
+++ src/main/java/io/jenkins/plugins/SignPath/Constants.java
@@ -4,5 +4,5 @@ package io.jenkins.plugins.SignPath;
  * Constants that are currently hard-coded (might become user-configurable in the future)
  */
 public class Constants {
-    public static final String TrustedBuildSystemTokenCredentialId = "TrustedBuildSystemToken";
+    public static final String TrustedBuildSystemTokenCredentialId = "SignPath.TrustedBuildSystemToken";
 }
