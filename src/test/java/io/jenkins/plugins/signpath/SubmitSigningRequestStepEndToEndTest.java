34f0f3862209d24d6b972e322b2f01b0b57a57d3
Volodymyr Bobko <volodymyr-bobko@users.noreply.github.com>
2024-06-28 11:53:27 +0200

    SIGN-6986 - fix unit tests

--- src/test/java/io/jenkins/plugins/signpath/SubmitSigningRequestStepEndToEndTest.java
+++ src/test/java/io/jenkins/plugins/signpath/SubmitSigningRequestStepEndToEndTest.java
@@ -321,7 +321,6 @@ public class SubmitSigningRequestStepEndToEndTest {
                 .withRequestBodyPart(aMultipart().withBody(equalTo(signingPolicySlug)).build())
                 .withRequestBodyPart(aMultipart().withBody(equalTo(remoteUrl)).build())
                 .withRequestBodyPart(aMultipart().withBody(equalTo(description)).build())
-                .withRequestBodyPart(aMultipart().withBody(equalTo(userDefinedParamValue)).build())
                 .withRequestBodyPart(aMultipart().withBody(equalTo(artifactConfigurationSlug)).build()));
 
         assertFormFiles(unsignedArtifactString, organizationId);
